871a60a17276bb8778a3648c55af44d32d65ee12
Jordi <jordi.colomer@philips.com>
2021-06-03 14:35:15 +0200

    adding login error

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -19,6 +19,7 @@
         placeholder="Email de usuari"
         required
         v-model="email"
+        @focus="isError = false"
       />
       <label for="password">Password</label>
       <input
@@ -27,7 +28,11 @@
         placeholder="Contrasenya"
         required
         v-model="password"
+        @focus="isError = false"
       />
+      <div class="error-message" v-show="isError">
+        Nom d'usuari o contrasenya incorrectes.<br />Torneu-ho a provar.
+      </div>
       <button type="submit" id="login">Login</button>
     </form>
   </main>
@@ -43,18 +48,24 @@ export default {
     return {
       email: "",
       password: "",
+      isError: false,
     };
   },
   methods: {
     onSubmit(event) {
       event.preventDefault();
+      document.getElementById("email").blur();
+      document.getElementById("password").blur();
       if (this.email !== "" && this.password !== "") {
         this.users.forEach((user) => {
           if (user.email === this.email && user.password === this.password) {
+            this.isError = false;
             console.log(`Yess!!!it is ${user.email}!`);
             this.$emit("do-logged-user", user);
             this.$emit("do-logged", true);
             this.$router.push({ name: "List" });
+          } else {
+            this.isError = true;
           }
         });
       } else {
