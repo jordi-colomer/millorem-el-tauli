ea4cf8e0c7ac4957cd31873dfc5e7fd0e0c66da3
Jordi <jordi.colomer@philips.com>
2021-06-04 09:19:36 +0200

    addingBE heroku url

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,11 @@ module.exports = {
   devServer: {
     proxy: {
       "^/api": {
-        target: "http://localhost:5000",
+        //target: "http://localhost:5000",
+        target: "https://millorem-el-tauli-be.herokuapp.com",
         changeOrigin: true,
         logLevel: "debug",
-        pathRewrite: { "^/api": "/" },
+        pathRewrite: { "^/api": "/api" },
       },
     },
   },
